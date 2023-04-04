3ffca01c23c35410ffeb7030d108810fac528971
Vitor Lopes <vitinhoolopes.vl@gmail.com>
2023-04-04 18:45:16 -0300

    using-js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,20 @@
+let editedPlayer = 0;
+
+const players = [
+    {
+        name: "",
+        symbol: "X"
+    },
+    {
+        name: "",
+        symbol: "O"
+    },
+];
+
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
@@ -12,3 +27,5 @@ editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener("click",closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
